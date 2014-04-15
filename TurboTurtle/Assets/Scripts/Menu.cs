c2fa5318f7968315ebc697d8bdc838a94e295d6e
Thepharaoh49 <Thepharaoh49@hotmail.co.uk>
2014-04-15 21:34:50 +0100

    Added Menu

---
+++ TurboTurtle/Assets/Scripts/Menu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+	
+	void OnGUI () {
+		GUI.backgroundColor = new Color(253,74,0);
+		if (GUI.Button (new Rect (280,240,400,100), "Start Game")) {
+			Application.LoadLevel("Ingame");
+		}
+		if (GUI.Button (new Rect (280,380,400,100), "Exit Game")) {
+			Application.Quit();
+		}
+	}
+}
